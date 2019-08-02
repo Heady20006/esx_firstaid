7fe869ed3871f056b449daf19d966cb7eb07bad1
abdulkadiraktas <abdulkadir.aktas@hotmail.com>
2019-08-03 00:50:19 +0300

    Create tr.lua

---
+++ locales/tr.lua
@@ -0,0 +1,7 @@
+    
+Locales['tr'] = {
+  ['no_license'] = 'İlk yardım sertifikanız yok!',
+  ['first_inprogress'] = 'İlk yardım yapılıyor...',
+  ['already_firstaid'] = 'İlk yardımı daha önceden yapılmış.',
+  ['done_firstaid'] = 'Elinden geleni yaptın şimdi beklemek zorundasın.',
+}
